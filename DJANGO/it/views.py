0fa7d222dc91a4f283cade0f548d628d079a70e9
rzh_exe <dima.rzhansky@gmail.com>
2025-09-17 11:41:06 +0500

    feat(it): добавлена главная страница с выводом постов
    Что сделано:
    - Создан шаблон templates/it/index.html для главной страницы.
    - В шаблоне отображается список постов с их категориями и заголовками.
    - В it/views.py создана функция index, которая:
        - Получает все объекты Post из базы
        - Передает их в шаблон через context с заголовком страницы
    - В it/urls.py добавлен путь '' → index.
    - В главном urls.py проекта добавлен include('it.urls') для маршрутизации на главную страницу.
    - Теперь при переходе на '/' отображается список всех постов из базы.

--- DJANGO/it/views.py
+++ DJANGO/it/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Post
 
-# Create your views here.
+def index(request):
+    """Главная страница"""
+    posts = Post.objects.all()
+    context = {
+        'title': 'Главная страница',
+        'posts': posts
+    }
+    return render(request, 'it/index.html', context)
