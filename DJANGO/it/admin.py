05b633dca0a32cd8a20caf8d904409c1a2a56be6
rzh_exe <dima.rzhansky@gmail.com>
2025-09-17 10:53:46 +0500

    chore(admin): добавлен Jazzmin и улучшена конфигурация админки для Post
    Что сделано:
    - В INSTALLED_APPS добавлено приложение 'jazzmin' для улучшенного интерфейса Django Admin.
    - Создан класс PostAdmin для модели Post с кастомными настройками:
        - list_display: отображаются id, категория, заголовок, количество просмотров, статус публикации, даты создания и обновления
        - list_display_links: кликабельными сделаны id и заголовок
        - list_editable: возможность редактировать поле is_published прямо из списка
        - readonly_fields: поле watched только для чтения
        - list_filter: фильтры по категории и публикации
    - Модель Post зарегистрирована с использованием PostAdmin, модель Category зарегистрирована стандартно.

--- DJANGO/it/admin.py
+++ DJANGO/it/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Category, Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'title', 'watched', 'is_published', 'created_at', 'updated_at']
+    list_display_links = ['id', 'title']
+    list_editable = ['is_published']
+    readonly_fields = ['watched']
+    list_filter = ['category', 'is_published']
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
