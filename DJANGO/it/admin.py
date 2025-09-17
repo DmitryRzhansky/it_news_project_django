6a8aa997db340ad7460817ad915ceb8223adbfc0
rzh_exe <dima.rzhansky@gmail.com>
2025-09-17 11:07:31 +0500

    chore(admin): добавлены миниатюры изображений для модели Post
    Что сделано:
    - В list_display добавлено поле thumbnail для отображения маленькой миниатюры фото прямо в списке постов.
    - В readonly_fields добавлено поле photo_preview для отображения миниатюры над полем photo при редактировании поста.
    - Методы thumbnail и photo_preview используют mark_safe для безопасного отображения HTML в админке.
    - Поля thumbnail и photo_preview имеют человеко-понятные заголовки (short_description).
    - Улучшена наглядность и удобство работы с изображениями в админке.

--- DJANGO/it/admin.py
+++ DJANGO/it/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Category, Post
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category', 'title', 'watched', 'is_published', 'created_at', 'updated_at']
+    list_display = ['id', 'category', 'title', 'thumbnail', 'watched', 'is_published', 'created_at', 'updated_at']
     list_display_links = ['id', 'title']
     list_editable = ['is_published']
-    readonly_fields = ['watched']
+    readonly_fields = ['watched', 'photo_preview']  # поле с превью
     list_filter = ['category', 'is_published']
+
+    def thumbnail(self, obj):
+        """Миниатюра в списке постов"""
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50" height="50" style="object-fit: cover; border-radius: 3px;" />')
+        return 'Нет фото'
+    thumbnail.short_description = 'Фото'
+
+    def photo_preview(self, obj):
+        """Миниатюра над полем photo в редакторе поста"""
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="150" height="150" style="object-fit: cover; border-radius: 5px; margin-bottom: 10px;" />')
+        return 'Нет фото'
+    photo_preview.short_description = 'Текущее изображение'
+
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
